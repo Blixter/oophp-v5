9de171b697ba6ca7fd87b667febc829f46a2ba15
Robin Blixter <r.blixter89@gmail.com>
2019-04-23 09:15:34 +0200

    Added a router-file for the dice100-game

---
+++ router/200_dice100.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Create routes using $app programming style.
+ */
+//var_dump(array_keys(get_defined_vars()));
+
+/**
+ * Init the game and redirect to play the game.
+ */
+$app->router->get("dice100/init", function () use ($app) {
+    // init the session for the gamestart.
+    $game = new Blixter\Dice\GameHandler();
+    $app->session->set("game", $game);
+    $app->session->set("lastRoll", null);
+    $app->session->set("roundTurn", null);
+    $app->session->set("choosedDices", false);
+    $roundTurn = $game->getRoundTurn();
+    $app->session->set("choice", "nothing");
+
+    return $app->response->redirect("dice100/play");
+});
+
+/**
+ * Play the game - show game status
+ */
+$app->router->get("dice100/play", function () use ($app) {
+    $app->session->set("lastRoundTurn", $app->session->get("roundTurn"));
+    $game = $app->session->get("game");
+    $title = "Play the game";
+    $app->session->set("roundTurn", $game->getRoundTurn());
+
+    $data = [
+        "title" => $title,
+    ];
+
+    $app->page->add("dice100/play", $data);
+
+    return $app->page->render([
+        "title" => $title,
+        ]);
+});
+
+
+/**
+ * Player roll the dices
+ */
+$app->router->post("dice100/player", function () use ($app) {
+    // Get the object saved to the session.
+    $game = $app->session->get("game");
+    $app->session->set("lastRoll", $game->playerTurn());
+    $app->session->set("checkRollOne", $game->player->checkIfRolledOne());
+
+    return $app->response->redirect("dice100/play");
+});
+
+/**
+ * Computer roll the dices
+ */
+$app->router->post("dice100/computer", function () use ($app) {
+    // Get the object saved to the session.
+    $game = $app->session->get("game");
+
+    $app->session->set("lastRoll", $game->computerTurn());
+    if ($game->computer->checkIfRolledOne() == false) {
+        $app->session->set("choice", $game->saveOrContinue());
+    };
+    return $app->response->redirect("dice100/play");
+});
+
+/**
+ * Player saves round score to total score
+ */
+$app->router->post("dice100/player-save", function () use ($app) {
+    // Get the object saved to the session.
+    $game = $app->session->get("game");
+
+    // Add current round score to players total score
+    $game->playerSaveScore();
+    // Redirect back to game
+    return $app->response->redirect("dice100/play");
+});
+
+/**
+* Chooses amount of dices
+*/
+$app->router->get("dice100/dices", function () use ($app) {
+    // Get the object saved to the session.
+    $game = $app->session->get("game");
+    $app->session->set("choosedDices", true);
+    $dices = intval($app->request->getGet("dices"));
+    $game->setNumberOfDices($dices);
+
+    // Redirect back to game
+    return $app->response->redirect("dice100/play");
+});
