ecc7c4c782135f8afe387d8e546e431e0dbb2894
Robin Blixter <r.blixter89@gmail.com>
2019-04-29 21:16:26 +0200

    Added the dice controller ro the router

---
+++ config/router/200_dice100.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Load the stylechooser as a controller class.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Dice controller",
+            "mount" => "dice100",
+            "handler" => "\Blixter\Dice\DiceController",
+        ],
+    ]
+];
